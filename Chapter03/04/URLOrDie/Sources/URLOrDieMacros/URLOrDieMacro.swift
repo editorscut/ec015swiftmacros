33d5b6f8250706c7d1aa7addc2b5f8385aa9402c
Daniel H Steinberg <inquiries@dimsumthinking.com>
2024-02-27 08:47:17 -0500

    Through 3.4

--- Chapter03/04/URLOrDie/Sources/URLOrDieMacros/URLOrDieMacro.swift
+++ Chapter03/04/URLOrDie/Sources/URLOrDieMacros/URLOrDieMacro.swift
@@ -3,6 +3,7 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import Foundation
+import UnwrapOrDie
 
 /// Implementation of the `URL` macro, which takes a String
 /// and produces a URL if a valid URL can be constructed
@@ -16,11 +17,15 @@ public struct URLOrDieMacro: ExpressionMacro {
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
     ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-
-        return "URL(string: \(argument))!"
+      let string = #unwrapOrDie(node.argumentList.first?.expression
+                                    .as(StringLiteralExprSyntax.self)?
+                                    .representedLiteralValue,
+                                message: "Argument must be a String literal")
+//      let _ = #unwrapOrDie(URL(string: string),
+//                           message: "Invalid URL")
+      return """
+        URL(string: "\(raw: string)")!
+        """
     }
 }
 
